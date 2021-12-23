221bd6742c4bb81b122c53972721f43e23d35ce6
seoyoung78 <tjdud0708@naver.com>
2021-12-23 11:33:41 +0900

    약속처방 목록

--- src/main/java/com/amaranth10/cr/cra/cra3000/mapper/CRA3000Mapper.java
+++ src/main/java/com/amaranth10/cr/cra/cra3000/mapper/CRA3000Mapper.java
@@ -1,7 +1,11 @@
 package com.amaranth10.cr.cra.cra3000.mapper;
 
+import com.amaranth10.cr.cra.cra3000.model.SetPrsc;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CRA3000Mapper {
+    List<SetPrsc> setPrscList (Object param);
 }
