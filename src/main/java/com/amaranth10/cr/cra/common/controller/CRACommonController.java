f17e6239d852a1ec112c7f9c8f571d48d8bc9a48
[헬스케어]이선형 <sunhyunglee@douzone.com>
2022-01-03 13:42:18 +0900

    공통영역 aop 적용

--- src/main/java/com/amaranth10/cr/cra/common/controller/CRACommonController.java
+++ src/main/java/com/amaranth10/cr/cra/common/controller/CRACommonController.java
@@ -1,183 +1,73 @@
 package com.amaranth10.cr.cra.common.controller;
 
-import com.amaranth10.backendcommon.util.helper.ExtractUtil;
-import com.amaranth10.backendcommon.util.model.SessionInfo;
 import com.amaranth10.backendcommon.util.model.APIResult;
-import com.amaranth10.cr.cra.common.service.impl.CRACommonServiceImpl;
-import com.amaranth10.cr.model.RequestModel;
-import com.amaranth10.cr.util.SessionManager;
-import klago.log.utils.RequestInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.amaranth10.cr.cra.common.model.Patient;
+import com.amaranth10.cr.cra.common.service.ICRACommonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 진료 공통 Controller
  */
-@Controller
+@RestController
 public class CRACommonController {
-    private static final Logger logger = LoggerFactory.getLogger(CRACommonController.class);
     @Autowired
-    private CRACommonServiceImpl craCommonServiceImpl;
-    @Autowired
-    SessionManager sessionManager;
+    private ICRACommonService iCRACommonService;
 
     /** 환자정보 조회 */
-    @RequestMapping(value = "/patientInfo", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public APIResult patientInfo(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.patientInfo(requestInfo, requestModel);
-        return result;
-    };
+    @PostMapping("/patientInfo")
+    public Object patientInfo (@RequestBody Object param) {
+        return this.iCRACommonService.patientInfo(param);
+    }
 
     /** 감염리스트 조회 */
-    @RequestMapping(value = "/infList", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public APIResult infList(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.infList(requestInfo, requestModel);
-        return result;
-    };
+    @PostMapping("/infList")
+    public Object infList (@RequestBody Object param) {
+        return this.iCRACommonService.infList(param);
+    }
 
     /** 환자감염정보 조회 */
-    @RequestMapping(value = "/patientInf", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public APIResult patientInf(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.patientInf(requestInfo, requestModel);
-        return result;
+    @PostMapping("/patientInf")
+    public Object patientInf (@RequestBody Object param) {
+        return this.iCRACommonService.patientInf(param);
     };
 
     /** 환자감염정보 추가 */
-    @RequestMapping(value = "/infInsert", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-    public APIResult infInsert(HttpServletRequest servletRequest,HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.infInsert(param);
-        return result;
+    @PostMapping("/infInsert")
+    public void infInsert (@RequestBody Object param) {
+        this.iCRACommonService.infInsert(param);
     }
 
     /** 환자감염정보 수정 */
-    @RequestMapping(value = "/infUpdate", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-    public APIResult infUpdate(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.infUpdate(param);
-        return result;
+    @PostMapping("/infUpdate")
+    public void infUpdate (@RequestBody Object param) {
+        this.iCRACommonService.infUpdate(param);
     }
 
     /** 환자알러지정보 조회 */
-    @RequestMapping(value = "/patientAlg", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public APIResult patientAlg(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.patientAlg(requestInfo, requestModel);
-        return result;
+    @PostMapping("/patientAlg")
+    public Object patientAlg (@RequestBody Object param) {
+        return this.iCRACommonService.patientAlg(param);
     };
 
     /** 환자알러지정보 추가 */
-    @RequestMapping(value = "/algInsert", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-    public APIResult algInsert(HttpServletRequest servletRequest,HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.algInsert(param);
-        return result;
+    @PostMapping("/algInsert")
+    public void algInsert (@RequestBody Object param) {
+        this.iCRACommonService.algInsert(param);
     }
 
     /** 환자알러지정보 수정 */
-    @RequestMapping(value = "/algUpdate", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-    public APIResult algUpdate(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.algUpdate(param);
-        return result;
+    @PostMapping("/algUpdate")
+    public void algUpdate (@RequestBody Object param) {
+        this.iCRACommonService.algUpdate(param);
     }
 
     /** 환자 접수상태 변경 */
-    @RequestMapping(value = "/updateRcpnStat", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-    public APIResult updateRcpnStat(HttpServletRequest servletRequest, HttpServletResponse servletResponse, @RequestBody Object param) throws Exception {
-        RequestInfo requestInfo = ExtractUtil.extractHeader(servletRequest, servletResponse);
-
-        SessionInfo userInfo = sessionManager.getSessionInfo(requestInfo.getAuthToken());
-
-        RequestModel requestModel = new RequestModel();
-        requestModel.setSessionInfo(userInfo);
-        requestModel.setData(param);
-
-        APIResult result = new APIResult();
-        result = craCommonServiceImpl.updateRcpnStat(param);
-        return result;
+    @PostMapping("/updateRcpnStat")
+    public void updateRcpnStat (@RequestBody Object param) {
+        this.iCRACommonService.updateRcpnStat(param);
     }
 
 }
