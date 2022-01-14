d4ecbd72ed62d211b8e93dc7432a2411171e6238
seoyoung78 <tjdud0708@naver.com>
2022-01-14 15:35:50 +0900

    처방내역 수정

--- src/main/java/com/amaranth10/cr/cra/common/model/Prescription.java
+++ src/main/java/com/amaranth10/cr/cra/common/model/Prescription.java
@@ -1,5 +1,10 @@
 package com.amaranth10.cr.cra.common.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Prescription {
     private String prsc_cd; // 처방코드
     private String prsc_nm; // 처방명
@@ -10,10 +15,12 @@ public class Prescription {
     private String nody; // 일수
     private String prsc_pay_dvcd; // 처방급여구분코드
     private String hsin_hsot_dvcd; // 원내/원외구분코드
-    private String hsin_prsc_resn_cd; // 원내처방사유코드
+    private String prsc_prgr_stat_cd; // 처방진행상태코드
+    private String rcpt_stat_cd; // 수납상태코드
     private String cmpt_yn; // 산정여부
     private String prsc_memo; // 처방메모
     private String jx999; // jx999
     private String dc_yn; // dc여부
-    private String dc_resn; // dc 사유
+    private String dc_resn_cd; // dc사유코드
+    private String dc_resn; // dc사유
 }
