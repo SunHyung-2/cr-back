7a59e3de03decf56640d098e6faf9ed4d5578bae
seoyoung78 <tjdud0708@naver.com>
2022-02-10 15:54:32 +0900

    진단, 처방 복사 기능 추가

--- src/main/java/com/amaranth10/cr/cra/cra1000/controller/CRA1000Controller.java
+++ src/main/java/com/amaranth10/cr/cra/cra1000/controller/CRA1000Controller.java
@@ -224,9 +224,7 @@ public class CRA1000Controller {
 
     /** 진단 내역 조회 */
     @PostMapping("/dgnsList")
-    public Object dgnsList (@RequestBody Object param) {
-        return this.iCRA1000Service.dgnsList(param);
-    }
+    public Object dgnsList (@RequestBody Object param) { return this.iCRA1000Service.dgnsList(param); }
 
     /** 진단 저장 */
     @PostMapping("/savePtDgns")
@@ -238,7 +236,7 @@ public class CRA1000Controller {
     @PostMapping("/updatePtDgns")
     public void updatePtDgns (@RequestBody Object param) {
         this.iCRA1000Service.updatePtDgns(param);
-        this.iCRA1000Service.savePtDgns(param);
+//        this.iCRA1000Service.savePtDgns(param);
     }
 
     /** 진단 형태 목록 조회 */
@@ -271,7 +269,7 @@ public class CRA1000Controller {
     @PostMapping("/updatePtPrsc")
     public void updatePtPrsc (@RequestBody Object param) {
         this.iCRA1000Service.updatePtPrsc(param);
-        this.iCRA1000Service.savePtPrsc(param);
+//        this.iCRA1000Service.savePtPrsc(param);
     }
 
     /** 의약분업예외 목록 조회 */
